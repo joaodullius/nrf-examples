7cf35a5e7b4c327176a52f8547abfa488f019d3c
Joao Dullius <joaodullius@bpmrep.com.br>
2023-01-02 09:28:44 -0300

    Added Button Characterisc to Peripheral
    Signed-off-by: Joao Dullius <joaodullius@bpmrep.com.br>

--- pairing_fixed_passkey/ble_custom_auth/src/main.c
+++ pairing_fixed_passkey/ble_custom_auth/src/main.c
@@ -24,10 +24,8 @@ static bool   notify_enabled;
 #define LOG_MODULE_NAME app
 LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 
-#define RUN_STATUS_LED          DK_LED1
+#define CHRC_STATUS_LED         DK_LED1
 #define CON_STATUS_LED			DK_LED2
-#define CHRC_STATUS_LED			DK_LED3
-#define RUN_LED_BLINK_INTERVAL  1000
 
 #define DEVICE_NAME CONFIG_BT_DEVICE_NAME
 #define DEVICE_NAME_LEN (sizeof(DEVICE_NAME)-1)
@@ -39,6 +37,10 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #define BT_UUID_CUSTOM_LED_CHAR_VAL \
    BT_UUID_128_ENCODE(0x86b50002, 0x7ff7, 0x496e, 0xaa9c, 0x05fc11855eb3)
 #define BT_UUID_CUSTOM_LED  BT_UUID_DECLARE_128(BT_UUID_CUSTOM_LED_CHAR_VAL)
+
+#define BT_UUID_BUTTON_CHRC_VAL \
+   BT_UUID_128_ENCODE(0x86b50003, 0x7ff7, 0x496e, 0xaa9c, 0x05fc11855eb3)
+#define BT_UUID_BUTTON_CHRC  BT_UUID_DECLARE_128(BT_UUID_BUTTON_CHRC_VAL)
 
 #define FIXED_PASSWORD 123456
 
@@ -67,23 +69,51 @@ static ssize_t on_write(struct bt_conn *conn, const struct bt_gatt_attr *attr,
     memcpy(temp_str, buf, len);
     temp_str[len] = 0x00;
 
-    LOG_DBG("Received data on conn %p. Len: %d", (void *)conn, len);
-	LOG_HEXDUMP_DBG(temp_str, len, "Received");
+    //LOG_DBG("Received data on conn %p. Len: %d", (void *)conn, len);
+	//LOG_HEXDUMP_DBG(temp_str, len, "Received");
 	
 	if (temp_str[0] == 0x01) {
-        LOG_DBG("Received value: 0x01, setting LED on\n");
+        LOG_DBG("Received value: 0x01, setting LED on");
 		dk_set_led_on(CHRC_STATUS_LED);
 
     } else if (temp_str[0] == 0x00) {
 
-        LOG_DBG("Received value: 0x00, setting LED off\n");
+        LOG_DBG("Received value: 0x00, setting LED off");
 		dk_set_led_off(CHRC_STATUS_LED);
 	}
 	return len;
 }
 
+void button_chrc_ccc_cfg_changed(const struct bt_gatt_attr *attr, uint16_t value)
+{
+   notify_enabled = (value == BT_GATT_CCC_NOTIFY);
+   LOG_INF("Notifications %s", notify_enabled? "enabled":"disabled");
+
+}
+
+
+static ssize_t read_button_chrc_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,
+			 void *buf, uint16_t len, uint16_t offset)
+{
+	LOG_DBG("Attribute read, handle: %u, conn: %p", attr->handle,
+		(void *)conn);
+
+	return bt_gatt_attr_read(conn, attr, buf, len, offset, &button_value,
+				 sizeof(button_value));
+}
+
 BT_GATT_SERVICE_DEFINE( custom_srv, 
                         BT_GATT_PRIMARY_SERVICE(BT_UUID_CUSTOM_SERVICE),
+
+						// BUTTON
+						BT_GATT_CHARACTERISTIC(	BT_UUID_BUTTON_CHRC,
+                    							BT_GATT_CHRC_READ | BT_GATT_CHRC_NOTIFY,
+                    							BT_GATT_PERM_READ_AUTHEN,
+                    							read_button_chrc_cb,
+												NULL, NULL),
+						BT_GATT_CCC(button_chrc_ccc_cfg_changed, BT_GATT_PERM_READ_AUTHEN | BT_GATT_PERM_WRITE_AUTHEN),
+						
+						//LED
 						BT_GATT_CHARACTERISTIC( BT_UUID_CUSTOM_LED,
                                                 BT_GATT_CHRC_WRITE,
                                                 BT_GATT_PERM_WRITE_AUTHEN,
@@ -215,39 +245,62 @@ static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
 	.pairing_failed = pairing_failed
 };
 
+int send_button_notification(struct bt_conn *conn, uint8_t value)
+{
+	if (!notify_enabled) {
+		return -EACCES;
+	}
+    int err = 0;
+
+	return bt_gatt_notify(NULL, &custom_srv.attrs[2],
+			      &button_value,
+			      sizeof(button_value));
+
+    struct bt_gatt_notify_params params = {0};
+    const struct bt_gatt_attr *attr = &custom_srv.attrs[2];
+
+    params.attr = attr;
+    params.data = &value;
+    params.len = 1;
+    params.func = on_sent;
+
+    err = bt_gatt_notify_cb(conn, &params);
+
+    return err;
+}
 
 
 void button_handler(uint32_t button_state, uint32_t has_changed)
 {
-    int err;
-	int button_pressed = 0;
-	if (has_changed & button_state)
+	uint8_t err;
+
+	if (has_changed)
 	{
 		switch (has_changed)
 		{
 			case DK_BTN1_MSK:
-				button_pressed = 1;
+				button_value = button_state;
+				err = send_button_notification(current_conn, button_value);
+        		if (err) {
+            		LOG_ERR("Couldn't send notificaton. (err: %d)", err);
+        		}
+				LOG_INF("Button 1 pressed - %d.", button_value);
 				break;
 			case DK_BTN2_MSK:
-				button_pressed = 2;
 				break;
 			case DK_BTN3_MSK:
-				button_pressed = 3;
 				break;
 			case DK_BTN4_MSK:
-				button_pressed = 4;
 				break;
 			default:
 				break;
 		}
-        LOG_INF("Button %d pressed.", button_pressed);
+        
     }
 }
 
-
 void main(void)
 {
-	int blink_status = 0;
 	int err;
 
 	LOG_INF("Starting Hello Bluetooth");
@@ -292,10 +345,5 @@ void main(void)
 	}
 
 	LOG_INF("Advertising successfully started");
-
-	for (;;) {
-		dk_set_led(RUN_STATUS_LED, (++blink_status) % 2);
-		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
-	}
 
 }
