068d79eeca6e2ecf627a036765b08dd895e48c1f
João Dullius <joaodullius@bpmrep.com.br>
2023-02-01 17:36:38 -0300

    Added Disable Uart Runtime
    Signed-off-by: João Dullius <joaodullius@bpmrep.com.br>

---
+++ disable_uart_runtime/src/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/logging/log_ctrl.h>
+
+void main(void)
+{
+	const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart0));
+	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	if (!device_is_ready(uart_dev)) {
+		return;
+	}
+
+	while(1)
+	{
+		pm_device_action_run(uart_dev, PM_DEVICE_ACTION_SUSPEND);
+		k_sleep(K_MSEC(2000));
+		printk("Should not be printed, since uart0 is disabled! %s\n", CONFIG_BOARD);
+		pm_device_action_run(uart_dev, PM_DEVICE_ACTION_RESUME);
+		k_sleep(K_MSEC(2000));
+		printk("Should be printed, since uart0 is enabled! %s\n", CONFIG_BOARD);
+	}	
+}
