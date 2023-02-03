f203a186db5bfc7bf268e5e1128a145aa1df2dec
João Dullius <joaodullius@bpmrep.com.br>
2023-02-03 09:01:18 -0300

    Timer GPPI GPIOTE Update
    Signed-off-by: João Dullius <joaodullius@bpmrep.com.br>

--- timer_gppi_gpiote/src/main.c
+++ timer_gppi_gpiote/src/main.c
@@ -1,41 +1,31 @@
-/*
- * Copyright (c) 2021 Joao Dulliuswest 
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <zephyr.h>
-#include <device.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
-#include <timing/timing.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/timing/timing.h>
 
 #include <nrfx_timer.h>
-#include <nrfx_dppi.h>
 #include <nrfx_gpiote.h>
+
+//GPPI + PPI/DPPI includes based on DPPI presence on platform
 #include <helpers/nrfx_gppi.h>
+#if defined(DPPI_PRESENT)
+	#include <nrfx_dppi.h>
+#else
+	#include <nrfx_ppi.h>
+#endif
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN_LED0	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS_LED0	DT_GPIO_FLAGS(LED0_NODE, gpios)
-
-#define LED_GPIOTE true
-
-#if !defined(LED_GPIOTE)
-	static bool led0_is_on = true;
-	static const struct device *dev_led0;
-#endif
+#define LED0_PIN	DT_GPIO_PIN(DT_ALIAS(led0), gpios)
 
 static const nrfx_timer_t m_sample_timer = NRFX_TIMER_INSTANCE(1);
 
 void timer_handler(nrf_timer_event_t event_type, void* p_context)
 {
-#if !defined(LED_GPIOTE)
-    gpio_pin_set(dev_led0, PIN_LED0, (int)led0_is_on);
-	led0_is_on = !led0_is_on;
-#endif
+	//Nothing here due to DPPI
 }
 
 static void timer_init(void)
@@ -44,7 +34,7 @@ static void timer_init(void)
 	timer_config.bit_width = NRF_TIMER_BIT_WIDTH_32;
     nrfx_err_t err_code = nrfx_timer_init(&m_sample_timer, &timer_config, timer_handler);
     if (err_code != NRFX_SUCCESS) {
-        printk("nrfx_timer_init error: %08x\n", err_code);
+        LOG_ERR("nrfx_timer_init error: %08x", err_code);
         return;
     }
     nrfx_timer_extended_compare(&m_sample_timer,
@@ -53,7 +43,7 @@ static void timer_init(void)
                                 NRF_TIMER_SHORT_COMPARE0_CLEAR_MASK,
                                 true);
     
-    printk("Timer 1 initialized.\n");
+    LOG_INF("Timer 1 initialized.");
     nrfx_timer_enable(&m_sample_timer);
 
 	
@@ -61,87 +51,87 @@ static void timer_init(void)
          nrfx_timer_1_irq_handler, NULL, 0);
 }
 
-#if !defined(LED_GPIOTE)
-static void gpio_init(void)
-{
-	dev_led0 = device_get_binding(LED0);
-	if (dev_led0 == NULL) {
-		return;
-	}
-
-	int ret = gpio_pin_configure(dev_led0, PIN_LED0, GPIO_OUTPUT_ACTIVE | FLAGS_LED0);
-	if (ret < 0) {
-		return;
-	}
-}
-#endif
-
-#if defined(LED_GPIOTE)
 static void gpiote_init(void)
 {
-	nrfx_gpiote_out_config_t led_pin_cfg = {0};
+	nrfx_err_t err;
+	uint8_t out_channel;
 
-	led_pin_cfg.action = NRF_GPIOTE_POLARITY_TOGGLE;
-	led_pin_cfg.init_state = NRF_GPIOTE_INITIAL_VALUE_HIGH;
-	led_pin_cfg.task_pin = true;
-
-	nrfx_err_t err = nrfx_gpiote_init(NRFX_GPIOTE_DEFAULT_CONFIG_IRQ_PRIORITY);
+	/* Initialize GPIOTE (the interrupt priority passed as the parameter
+	 * here is ignored, see nrfx_glue.h).
+	 */
+	err = nrfx_gpiote_init(0);
 	if (err != NRFX_SUCCESS) {
-		printk("gpiote1 err: %x", err);
+		LOG_ERR("nrfx_gpiote_init error: 0x%08X", err);
 		return;
 	}
 
-	err = nrfx_gpiote_out_init(PIN_LED0, &led_pin_cfg);
+	err = nrfx_gpiote_channel_alloc(&out_channel);
 	if (err != NRFX_SUCCESS) {
-		printk("gpiote1 err: %x", err);
+		LOG_ERR("Failed to allocate out_channel, error: 0x%08X", err);
+		return;
+	}
+	
+	/* Initialize output pin. SET task will turn the LED on,
+	 * CLR will turn it off and OUT will toggle it.
+	 */
+	static const nrfx_gpiote_output_config_t output_config = {
+		.drive = NRF_GPIO_PIN_S0S1,
+		.input_connect = NRF_GPIO_PIN_INPUT_DISCONNECT,
+		.pull = NRF_GPIO_PIN_NOPULL,
+	};
+	const nrfx_gpiote_task_config_t task_config = {
+		.task_ch = out_channel,
+		.polarity = NRF_GPIOTE_POLARITY_TOGGLE,
+		.init_val = 1,
+	};
+	err = nrfx_gpiote_output_configure(LED0_PIN,
+					   &output_config,
+					   &task_config);
+	if (err != NRFX_SUCCESS) {
+		LOG_ERR("nrfx_gpiote_output_configure error: 0x%08X", err);
 		return;
 	}
 
-	nrfx_gpiote_out_task_enable(PIN_LED0);
+	nrfx_gpiote_out_task_enable(LED0_PIN);
+
+	LOG_INF("nrfx_gpiote initialized");
+
 }
 
-static void dppi_init(void)
+
+static void gppi_init(void)
 {
-	uint8_t dppi_ch_1;
-	nrfx_err_t err = nrfx_dppi_channel_alloc(&dppi_ch_1);
+	nrfx_err_t err;
+	uint8_t ppi_channel;
+
+	/* Allocate a (D)PPI channel. */
+	err = nrfx_gppi_channel_alloc(&ppi_channel);
 	if (err != NRFX_SUCCESS) {
-		printk("Err %d\n", err);
+		LOG_ERR("nrfx_gppi_channel_alloc error: 0x%08X", err);
 		return;
 	}
 
-	err = nrfx_dppi_channel_enable(dppi_ch_1);
-	if (err != NRFX_SUCCESS) {
-		printk("Err %d\n", err);
-		return;
-	}
+	/* Configure a (D)PPI channel to toggle the LED0 pin on TIMER0 COMPARE[0] match. */
+	nrfx_gppi_channel_endpoints_setup(ppi_channel,
+		nrf_timer_event_address_get(m_sample_timer.p_reg, NRF_TIMER_EVENT_COMPARE0),
+		nrfx_gpiote_out_task_addr_get(LED0_PIN));
 
-	/* Tie it all together */
-	//nrf_timer_publish_set(m_sample_timer.p_reg, NRF_TIMER_EVENT_COMPARE0, dppi_ch_1);
-	//nrf_gpiote_subscribe_set(led_pin.p_reg, NRF_GPIOTE_TASK_OUT_0, dppi_ch_1);
-
-
-	nrfx_gppi_channel_endpoints_setup(dppi_ch_1,
-			nrf_timer_event_address_get(m_sample_timer.p_reg,
-				NRF_TIMER_EVENT_COMPARE0),
-			nrf_gpiote_task_address_get(NRF_GPIOTE,
-				nrfx_gpiote_in_event_get(PIN_LED0)));
-
+	/* Enable the channel. */
+	nrfx_gppi_channels_enable(BIT(ppi_channel));
 }
-#endif
+
 
 void main(void)
 {
-    printk("Starting Fancy Blinker Application...\n");
-	timer_init();
+	#if defined(DPPI_PRESENT)
+    	LOG_INF("Starting Timer + DPPI + GPIOTE Application...");
+	#else
+		LOG_INF("Starting Timer + PPI + GPIOTE Application...");
+	#endif
 
-#if defined(LED_GPIOTE)
+	timer_init();
 	gpiote_init();
-	dppi_init();
-	printk("Timer + DPPI + GPIOTE Mode\n");
-#else
-	gpio_init();
-	printk("Timer + GPIO Mode\n");
-#endif    
+	gppi_init();
 
 }
 
