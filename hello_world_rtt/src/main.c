297ee8bfa4002044fe4c42c72209fcdd7c103edd
João Dullius <joaodullius@bpmrep.com.br>
2025-08-12 18:13:45 -0300

    V3.0.2

--- hello_world_rtt/src/main.c
+++ hello_world_rtt/src/main.c
@@ -6,7 +6,10 @@
 
 #include <zephyr/kernel.h>
 
-void main(void)
-{
-	printk("Hello World! %s\n", CONFIG_BOARD);
+int main(void)
+{	
+	while(1)
+	{
+		printk("Hello RTT! %s\n", CONFIG_BOARD);
+	}
 }
